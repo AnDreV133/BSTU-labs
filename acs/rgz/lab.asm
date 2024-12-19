a6c64ddc884aa2e94a1285a75853a582966780a3
AnDreV133 <dmitrijkubik@gmail.com>
2024-12-19 17:07:06 +0300

    change

--- acs/rgz/lab.asm
+++ acs/rgz/lab.asm
@@ -16,11 +16,10 @@ WinMain proto :DWORD,:DWORD,:DWORD,:DWORD
 
 .data
 ClassName db "SimpleWinClass",0
-AppName  db "Triangle Example",0
+AppName  db "IITUS",0
 
-.data?
 hInstance HINSTANCE ?
-hPen     HPEN ? ; Дескриптор для пера
+hPen     HPEN ? 
 
 .code
 start:
@@ -69,13 +68,9 @@ WinMain endp
 WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
     LOCAL hdc:HDC
     LOCAL ps:PAINTSTRUCT
-    LOCAL hOldPen:HPEN     ; Для хранения старого пера
-    LOCAL hBluePen:HPEN    ; Для синего пера
-    LOCAL hRedPen:HPEN
-    LOCAL hWhitePen:HPEN
-    LOCAL pointsOfShape[6]:POINT    ; Точки пятиугольника
-    LOCAL pointsOfLines[3]:POINT   ; Точки линий
-    LOCAL pointsOfEyelet[10]:POINT  ; Точки петелек 
+    LOCAL hMainPen:HPEN    
+    LOCAL hSubPen:HPEN
+    LOCAL points[6]:POINT   
 
     .IF uMsg == WM_DESTROY
         invoke PostQuitMessage, 0
@@ -83,103 +78,187 @@ WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
         invoke BeginPaint, hWnd, ADDR ps
         mov hdc, eax
 
-        ; Определяем точки пятиугольника
-        mov pointsOfShape[0 * TYPE POINT].x, 110
-        mov pointsOfShape[0 * TYPE POINT].y, 80
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ; пятиугольник
+        mov points[0 * TYPE POINT].x, 110
+        mov points[0 * TYPE POINT].y, 80
 
-        mov pointsOfShape[1 * TYPE POINT].x, 150
-        mov pointsOfShape[1 * TYPE POINT].y, 110
+        mov points[1 * TYPE POINT].x, 150
+        mov points[1 * TYPE POINT].y, 110
 
-        mov pointsOfShape[2 * TYPE POINT].x, 135
-        mov pointsOfShape[2 * TYPE POINT].y, 155
+        mov points[2 * TYPE POINT].x, 135
+        mov points[2 * TYPE POINT].y, 155
 
-        mov pointsOfShape[3 * TYPE POINT].x, 85
-        mov pointsOfShape[3 * TYPE POINT].y, 155
+        mov points[3 * TYPE POINT].x, 85
+        mov points[3 * TYPE POINT].y, 155
 
-        mov pointsOfShape[4 * TYPE POINT].x, 70
-        mov pointsOfShape[4 * TYPE POINT].y, 110
+        mov points[4 * TYPE POINT].x, 70
+        mov points[4 * TYPE POINT].y, 110
 
-        mov pointsOfShape[5 * TYPE POINT].x, 110
-        mov pointsOfShape[5 * TYPE POINT].y, 80
+        mov points[5 * TYPE POINT].x, 110
+        mov points[5 * TYPE POINT].y, 80
 
-        ; Красный цвет
-        invoke CreatePen, PS_SOLID, 10, 0FFh 
-        mov hRedPen, eax
+        invoke CreatePen, PS_SOLID, 10, 02B26B1h 
+        mov hMainPen, eax
 
-        ; Выбираем красное перо в контекст устройства
-        invoke SelectObject, hdc, hRedPen
-        mov hOldPen, eax ; Сохраняем старое перо
+        invoke SelectObject, hdc, hMainPen
 
-        ; Рисуем пятиугольник без заполнения
-        invoke Polyline, hdc, ADDR pointsOfShape, 6
+        invoke Polyline, hdc, ADDR points, 6
 
-        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ; линии
+        invoke CreatePen, PS_SOLID, 10, 0563012h 
+        mov hMainPen, eax
 
-        ; Создаем синее перо
-        invoke CreatePen, PS_SOLID, 10, 0FF0000h 
-        mov hBluePen, eax
-
-        ; Выбираем синее перо в контекст устройства
-        invoke SelectObject, hdc, hBluePen
-        mov hOldPen, eax
+        invoke SelectObject, hdc, hMainPen
 
         ; 1
-        mov pointsOfLines[0 * TYPE POINT].x, 130
-        mov pointsOfLines[0 * TYPE POINT].y, 64
+        mov points[0 * TYPE POINT].x, 130
+        mov points[0 * TYPE POINT].y, 64
 
-        mov pointsOfLines[1 * TYPE POINT].x, 195
-        mov pointsOfLines[1 * TYPE POINT].y, 113
+        mov points[1 * TYPE POINT].x, 197
+        mov points[1 * TYPE POINT].y, 113
 
-        mov pointsOfLines[2 * TYPE POINT].x, 163
-        mov pointsOfLines[2 * TYPE POINT].y, 216
+        mov points[2 * TYPE POINT].x, 163
+        mov points[2 * TYPE POINT].y, 216
 
-        invoke Polyline, hdc, ADDR pointsOfLines, 3
+        invoke Polyline, hdc, ADDR points, 3
 
         ; 2
-        mov pointsOfLines[0 * TYPE POINT].x, 173
-        mov pointsOfLines[0 * TYPE POINT].y, 120
+        mov points[0 * TYPE POINT].x, 172
+        mov points[0 * TYPE POINT].y, 120
 
-        mov pointsOfLines[1 * TYPE POINT].x, 145
-        mov pointsOfLines[1 * TYPE POINT].y, 197
+        mov points[1 * TYPE POINT].x, 145
+        mov points[1 * TYPE POINT].y, 197
 
-        mov pointsOfLines[2 * TYPE POINT].x, 37
-        mov pointsOfLines[2 * TYPE POINT].y, 197
+        mov points[2 * TYPE POINT].x, 37
+        mov points[2 * TYPE POINT].y, 197
 
-        invoke Polyline, hdc, ADDR pointsOfLines, 3
+        invoke Polyline, hdc, ADDR points, 3
         
         ; 3
-        mov pointsOfLines[0 * TYPE POINT].x, 132
-        mov pointsOfLines[0 * TYPE POINT].y, 176
+        mov points[0 * TYPE POINT].x, 130
+        mov points[0 * TYPE POINT].y, 176
 
-        mov pointsOfLines[1 * TYPE POINT].x, 50
-        mov pointsOfLines[1 * TYPE POINT].y, 176
+        mov points[1 * TYPE POINT].x, 49
+        mov points[1 * TYPE POINT].y, 176
 
-        mov pointsOfLines[2 * TYPE POINT].x, 13
-        mov pointsOfLines[2 * TYPE POINT].y, 74
+        mov points[2 * TYPE POINT].x, 13
+        mov points[2 * TYPE POINT].y, 74
 
-        invoke Polyline, hdc, ADDR pointsOfLines, 3
+        invoke Polyline, hdc, ADDR points, 3
 
         ; 4
-        mov pointsOfLines[0 * TYPE POINT].x, 132
-        mov pointsOfLines[0 * TYPE POINT].y, 176
+        mov points[0 * TYPE POINT].x, 63
+        mov points[0 * TYPE POINT].y, 156
 
-        mov pointsOfLines[1 * TYPE POINT].x, 50
-        mov pointsOfLines[1 * TYPE POINT].y, 176
+        mov points[1 * TYPE POINT].x, 38
+        mov points[1 * TYPE POINT].y, 81
 
-        mov pointsOfLines[2 * TYPE POINT].x, 13
-        mov pointsOfLines[2 * TYPE POINT].y, 74
+        mov points[2 * TYPE POINT].x, 125
+        mov points[2 * TYPE POINT].y, 14
 
-        invoke Polyline, hdc, ADDR pointsOfLines, 3
+        invoke Polyline, hdc, ADDR points, 3
 
-        ; Восстанавливаем старое перо
-        invoke SelectObject, hdc, hOldPen
+        ; 5
+        mov points[0 * TYPE POINT].x, 63
+        mov points[0 * TYPE POINT].y, 88
 
-        ; Удаляем красное перо
-        invoke DeleteObject, hRedPen
+        mov points[1 * TYPE POINT].x, 131
+        mov points[1 * TYPE POINT].y, 38
 
-        ; Удаляем синее перо
-        invoke DeleteObject, hBluePen
+        mov points[2 * TYPE POINT].x, 219
+        mov points[2 * TYPE POINT].y, 101
 
+        invoke Polyline, hdc, ADDR points, 3
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ; петельки
+        invoke CreatePen, PS_SOLID, 24, 0563012h 
+        mov hMainPen, eax
+
+        invoke CreatePen, PS_SOLID, 11, 0FFFFFFh 
+        mov hSubPen, eax
+
+        ; 1
+        mov points[0 * TYPE POINT].x, 163
+        mov points[0 * TYPE POINT].y, 216 
+
+        mov eax, points[0 * TYPE POINT].x
+        mov points[1 * TYPE POINT].x, eax
+        mov eax, points[0 * TYPE POINT].y
+        mov points[1 * TYPE POINT].y, eax
+
+        invoke SelectObject, hdc, hMainPen
+        invoke Polyline, hdc, ADDR points, 2  
+
+        invoke SelectObject, hdc, hSubPen
+        invoke Polyline, hdc, ADDR points, 2 
+
+        ; 2
+        mov points[0 * TYPE POINT].x, 37
+        mov points[0 * TYPE POINT].y, 197
+
+        mov eax, points[0 * TYPE POINT].x
+        mov points[1 * TYPE POINT].x, eax
+        mov eax, points[0 * TYPE POINT].y
+        mov points[1 * TYPE POINT].y, eax
+
+        invoke SelectObject, hdc, hMainPen
+        invoke Polyline, hdc, ADDR points, 2  
+
+        invoke SelectObject, hdc, hSubPen
+        invoke Polyline, hdc, ADDR points, 2 
+
+        ; 3
+        mov points[0 * TYPE POINT].x, 13
+        mov points[0 * TYPE POINT].y, 74 
+
+        mov eax, points[0 * TYPE POINT].x
+        mov points[1 * TYPE POINT].x, eax
+        mov eax, points[0 * TYPE POINT].y
+        mov points[1 * TYPE POINT].y, eax
+
+        invoke SelectObject, hdc, hMainPen
+        invoke Polyline, hdc, ADDR points, 2  
+
+        invoke SelectObject, hdc, hSubPen
+        invoke Polyline, hdc, ADDR points, 2 
+
+        ; 4
+        mov points[0 * TYPE POINT].x, 125
+        mov points[0 * TYPE POINT].y, 14 
+
+        mov eax, points[0 * TYPE POINT].x
+        mov points[1 * TYPE POINT].x, eax
+        mov eax, points[0 * TYPE POINT].y
+        mov points[1 * TYPE POINT].y, eax
+
+        invoke SelectObject, hdc, hMainPen
+        invoke Polyline, hdc, ADDR points, 2  
+
+        invoke SelectObject, hdc, hSubPen
+        invoke Polyline, hdc, ADDR points, 2 
+
+        ; 5
+        mov points[0 * TYPE POINT].x, 219
+        mov points[0 * TYPE POINT].y, 101  
+
+        mov eax, points[0 * TYPE POINT].x
+        mov points[1 * TYPE POINT].x, eax
+        mov eax, points[0 * TYPE POINT].y
+        mov points[1 * TYPE POINT].y, eax
+
+        invoke SelectObject, hdc, hMainPen
+        invoke Polyline, hdc, ADDR points, 2  
+
+        invoke SelectObject, hdc, hSubPen
+        invoke Polyline, hdc, ADDR points, 2  
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ; конец отрисовки
+        invoke DeleteObject, hMainPen
+        invoke DeleteObject, hSubPen
         invoke EndPaint, hWnd, ADDR ps
     .ELSE
         invoke DefWindowProc, hWnd, uMsg, wParam, lParam
