32683676dcdf201f484d86d3a960d595db6d1707
AnDreV133 <dmitrijkubik@gmail.com>
2025-02-17 20:08:15 +0300

    change

--- 3kurs2sem/МетодыАД/lab1/2.py
+++ 3kurs2sem/МетодыАД/lab1/2.py
@@ -4,27 +4,37 @@ from scipy.stats import expon, kstest
 
 lambda_param = 2 / 4
 sample_size = 10000
-intervals = 15
+intervals = 30
 
 sample = np.random.exponential(size=sample_size)
 
-x = np.linspace(0, 10, sample_size)
-y = expon.pdf(x)
+# Теоретические значения
+theoretical_mean = 1 / lambda_param
+theoretical_var = 1 / (lambda_param ** 2)
 
-sample_avgs = []
-y_avgs = []
-interval_length = int(sample_size / intervals)
-for i in range(interval_length, sample_size, interval_length):
-    sample_avgs.append(sum(sample[i - interval_length:i]) / interval_length)
-    y_avgs.append(sum(y[i - interval_length:i]) / interval_length)
+# Выборочные характеристики
+sample_mean = np.mean(sample)
+sample_var = np.var(sample)
+print(f"Теоретическое среднее: {theoretical_mean:.4f}")
+print(f"Выборочное среднее: {sample_mean:.4f}")
+print(f"Теоретическая дисперсия: {theoretical_var:.4f}")
+print(f"Выборочная дисперсия: {sample_var:.4f}")
 
-alpha = 0.5
-print(list(zip(sample_avgs, y_avgs, map(lambda x: abs(x[0] - x[1]) < alpha, zip(sample_avgs, y_avgs)))))
+# Тест Колмогорова-Смирнова
+ks_statistic, p_value = kstest(sample, 'expon', args=(0, 1))
+print(f"K-S статистика: {ks_statistic:.4f}")
+print(f"P-значение: {p_value:.4f}")
 
-if all(map(lambda x: abs(x[0] - x[1]) < alpha, zip(sample_avgs, y_avgs))):
+# Интерпретация p-значения
+alpha = 0.05
+if p_value > alpha:
     print("Соответсвует распределению")
 else:
     print("Не соответствует распределению")
+
+
+x = np.linspace(0, 10, sample_size)
+y = expon.pdf(x)
 
 plt.hist(sample, bins=intervals, density=True, alpha=0.6, color='g', label='Гистограмма частот')
 plt.plot(x, y, 'r-', label='Теоретическая плотность')
@@ -33,20 +43,3 @@ plt.xlabel('Значения')
 plt.ylabel('Плотность')
 plt.legend()
 plt.show()
-
-# Вычисление среднего арифметического
-# sample_avg = sum(sample) / sample_size
-# y_avg = sum(y) / len(x)
-# print(f'Среднее арифметическое значений выборки: {sample_avg:.4f} - {y_avg:.4f}')
-
-
-# # Проверка соответствия распределения с использованием критерия Колмогорова-Смирнова
-# ks_statistic, p_value = kstest(sample, 'expon', args=(0, 1))
-# print(f'K-S статистика: {ks_statistic:.4f}, p-значение: {p_value:.4f}')
-
-# Проверка на уровне значимости 0.05
-# alpha = 0.05
-# if p_value > alpha:
-#     print("Соответсвует распределению")
-# else:
-#     print("Не соответствует распределению")
